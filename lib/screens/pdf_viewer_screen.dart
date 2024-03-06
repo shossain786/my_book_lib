1d936facc750898d05de22752871ad8763994e31
Saddam <sddmhossain@786gmail.com>
2024-03-06 10:57:30 +0530

    pdf read code updated

--- lib/screens/pdf_viewer_screen.dart
+++ lib/screens/pdf_viewer_screen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PdfViewerScreen extends StatefulWidget {
   final String pdfPath;
@@ -14,24 +17,41 @@ class _PdfViewerScreenState extends State<PdfViewerScreen> {
   late PDFViewController pdfController;
   int pageNumber = 1;
   bool nightMode = false;
+  late TextEditingController pageController;
+  late Color hintColor;
+  late Color inputColor;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = TextEditingController();
+    hintColor = Colors.black54;
+    inputColor = Colors.black;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: nightMode ? Colors.black : Colors.white,
       appBar: AppBar(
-        title: const Text('PDF Viewer'),
+        title: const Text(
+          'PDF Viewer',
+          // style: TextStyle(color: nightMode ? Colors.white : Colors.black),
+        ),
+        foregroundColor: nightMode ? Colors.white : Colors.black,
+        backgroundColor: nightMode ? Colors.black : Colors.white,
         actions: [
           IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              // Add search functionality here
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.lightbulb),
+            icon: Icon(
+              Icons.lightbulb,
+              color: nightMode
+                  ? const Color.fromARGB(255, 64, 244, 70)
+                  : Colors.black,
+            ),
             onPressed: () {
               setState(() {
                 nightMode = !nightMode;
+                _updateTextFieldColors();
               });
             },
           ),
@@ -57,71 +77,120 @@ class _PdfViewerScreenState extends State<PdfViewerScreen> {
           ),
         ],
       ),
-      body: Stack(
-        children: [
-          PDFView(
-            filePath: widget.pdfPath,
-            autoSpacing: true,
-            enableSwipe: true,
-            fitEachPage: true,
-            fitPolicy: FitPolicy.WIDTH,
-            nightMode: nightMode,
-            onRender: (pages) {
-              setState(() {
-                pageNumber = pages!;
-              });
-            },
-            onViewCreated: (PDFViewController controller) {
-              setState(() {
-                pdfController = controller;
-              });
-            },
-            onPageChanged: (int? page, int? total) {
-              setState(() {
-                pageNumber = page! + 1;
-              });
-            },
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              color: nightMode ? Colors.black54 : Colors.white54,
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back),
-                    onPressed: () {
-                      if (pageNumber > 1) {
-                        pdfController.setPage(--pageNumber - 1);
-                      }
-                    },
-                  ),
-                  Text(
-                    'Page $pageNumber',
-                    style: TextStyle(
+      body: Container(
+        color: nightMode ? Colors.black : Colors.white,
+        child: Stack(
+          children: [
+            PDFView(
+              filePath: widget.pdfPath,
+              autoSpacing: true,
+              enableSwipe: true,
+              fitEachPage: true,
+              fitPolicy: FitPolicy.WIDTH,
+              nightMode: nightMode,
+              onRender: (pages) {
+                setState(() {
+                  pageNumber = pages!;
+                });
+              },
+              onViewCreated: (PDFViewController controller) {
+                setState(() {
+                  pdfController = controller;
+                });
+              },
+              onPageChanged: (int? page, int? total) {
+                setState(() {
+                  pageNumber = page! + 1;
+                });
+              },
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: nightMode ? Colors.black : Colors.white,
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back),
                       color: nightMode ? Colors.white : Colors.black,
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.arrow_forward),
-                    onPressed: () {
-                      pdfController.getPageCount().then((count) {
-                        if (pageNumber < count!) {
-                          pdfController.setPage(++pageNumber - 1);
+                      onPressed: () {
+                        if (pageNumber > 1) {
+                          pdfController.setPage(--pageNumber - 1);
                         }
-                      });
-                    },
-                  ),
-                ],
+                      },
+                    ),
+                    Text(
+                      'Page $pageNumber',
+                      style: TextStyle(
+                        color: nightMode ? Colors.white : Colors.black,
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_forward),
+                      color: nightMode ? Colors.white : Colors.black,
+                      onPressed: () {
+                        pdfController.getPageCount().then((count) {
+                          if (pageNumber < count!) {
+                            pdfController.setPage(++pageNumber - 1);
+                          }
+                        });
+                      },
+                    ),
+                    const SizedBox(width: 20),
+                    SizedBox(
+                      width: 60,
+                      child: TextField(
+                        controller: pageController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'Page',
+                          hintStyle: TextStyle(color: hintColor),
+                        ),
+                        style: TextStyle(color: inputColor),
+                      ),
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        iconColor: MaterialStateColor.resolveWith((states) =>
+                            nightMode ? Colors.white : Colors.black),
+                      ),
+                      onPressed: () {
+                        int? page = int.tryParse(pageController.text);
+                        if (page != null && page > 0) {
+                          pdfController.setPage(page - 1);
+                          setState(() {
+                            pageNumber = page;
+                          });
+                        } else {
+                          // Handle invalid input
+                        }
+                      },
+                      child: const FaIcon(FontAwesomeIcons.anglesRight),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  void _updateTextFieldColors() {
+    setState(() {
+      hintColor = nightMode ? Colors.white70 : Colors.black54;
+      inputColor = nightMode ? Colors.white : Colors.black;
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
   }
 }
---
+++ lib/screens/test.dart
@@ -0,0 +1,30 @@
+// import 'dart:ffi';
+
+// import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Icon examples'),
+//       ),
+//       body: const Center(
+//         child: Column(
+//           children: [
+//             Text('My Widgets!'),
+//             SizedBox(height: 20),
+//             FaIcon(FontAwesomeIcons.gofore),
+//             SizedBox(height: 10),
+//             FaIcon(FontAwesomeIcons.diagramNext),
+//             SizedBox(height: 10),
+//             FaIcon(FontAwesomeIcons.bilibili),
+//             SizedBox(height: 10),
+//             FaIcon(FontAwesomeIcons.anglesRight),
+//             SizedBox(height: 10),
+//           ],
+//         ),
+//       ),
+//     ),
+//   ));
+// }
